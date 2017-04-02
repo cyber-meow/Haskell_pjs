76ca2e620ee9db4b858237e8ffc3d85a27b4d6c2
cyber-meow <sjungle305@gmail.com>
2017-04-02 22:48:52 +0800

    bytestring format

--- encoding_algorithm/Main.hs
+++ encoding_algorithm/Main.hs
@@ -6,6 +6,11 @@ import System.IO
 import Data.Map (Map)
 import qualified Data.Map as Map
 
+import Data.Char
+import Data.Word
+import Data.Digits (unDigits)
+import qualified Data.ByteString as B
+
 import Huffman
 
 -- count the number of occurence of each letter in an ASCII file
@@ -25,12 +30,21 @@ coding :: String -> Maybe String
 coding str = fmap concat $  sequence $ map (\c -> Map.lookup c codebook) str
  where codebook = huffmanCoding . countFrequency $ str
 
+groupWord :: String -> [Word8]
+groupWord [] = []
+groupWord s = fromIntegral (unDigits 2 (map digitToInt hd)) : groupWord tl
+  where (hd, tl) = splitAt 8 s
+
+-- convert a string of 0 and 1 to byte string
+toByteString :: String -> B.ByteString
+toByteString = B.pack . groupWord
+
 main = do
   args <- getArgs
   fileContent <- readFile $ head args  
   case coding fileContent of
     Nothing -> hPutStrLn stderr
       "something wrong while trying to coding the source"
-    Just code -> writeFile (args!!1) code
+    Just code -> B.writeFile (args!!1) (toByteString code)
 
 
